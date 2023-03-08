6152812cbeffb3114e75f29552e1cff9aaa82a0c
https://github.com/juayuohcarineneng19/ <juayuoh@gmail.com>
2023-03-08 18:52:15 +0100

    responsive navbar

--- src/Components/head/Navbar.jsx
+++ src/Components/head/Navbar.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
+  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faBars } from '@fortawesome/free-solid-svg-icons'
+
 import './Navbar.css'
 
 function Navbar() {
+  // const toggleButton = document.getElementByClassName("toggle-button")[0]
+  // const mainNavList = document.getElementsByClassName("main-nav-list")[0]
+  // // const navButtons = document.getElementsByClassName("navbuttons")[0]
+
+  // toggleButton.addEventListener('click',() =>{
+  //  mainNavList.classList.toggle('active')
+  //   // navButtons.classList.toggle('active')
+  // })
   return (
     <div className='navbar'>
         <div className="logo">
         <a href="#">Logo</a>
       </div>
       <div className="main-nav">
-      <ul className="main-nav-list">
+      <ul className="main-nav-list ">
         <li><a href="#" className="main-nav-link">Features</a></li>
         <li><a href="#" className="main-nav-link">Company</a></li>
         <li><a href="#" className="main-nav-link">Community</a></li>
@@ -18,10 +30,17 @@ function Navbar() {
         
       </ul>
       </div>
-      <div className="navbuttons">
+      <div className="navbuttons ">
       <a href="#" class="btn btn--outline margin-right-sm  "><strong> Login </strong></a>
          <a href="#" class="btn btn--full ">Get Started</a>
       </div>
+      
+      {/* menu bars */}
+      <a href="#" className="toggle-button">
+        <span className="bars"></span>
+        <span className="bars"></span>
+        <span className="bars"></span>
+      </a>
     </div>
   )
 }
