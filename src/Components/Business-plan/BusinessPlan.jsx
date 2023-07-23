362c4e42ee9b8092220a23f0557adb3c82f60cb7
juayuohcarineneng19 <juayuoh@gmail.com>
2023-07-23 22:57:18 +0100

    C:/Program Files/Git/

--- src/Components/Business-plan/BusinessPlan.jsx
+++ src/Components/Business-plan/BusinessPlan.jsx
@@ -16,8 +16,6 @@ function BusinessPlan() {
             <h1>Choose a Bumpa plan</h1>
             <p>There’s a Bumpa plan that fits your business.</p>
         </div>
-
-
     <div className="card-wrapper">
         <div className="card">
         <h5 className="card-title"> Bumpa Basic </h5>
@@ -35,8 +33,48 @@ function BusinessPlan() {
             <li> <span style={iconStyles}><FaCheck/></span>Business Analytics</li>
             <li> <span style={iconStyles}><FaCheck/></span>Free .bumpa.shop domain</li>
           </ul>
-          <a href="/" class="btn  btn--full">Get Started</a>
+          <a href="/" class="btn  btn--full btn--busines">Get Started</a>
+    </div>     
+
+    <div className="card">
+        <h5 className="card-title"> Bumpa Basic </h5>
+        <p className="card-price"><span class="price-tag">NO</span>/month</p>
+          <p className="description">For new businesses</p>
+          <hr className="line" />
+          <ul className="card-list">
+            <li> <span style={iconStyles}><FaCheck/></span>Free business website</li>
+            <li> <span style={iconStyles}><FaCheck/></span>Online transaction charge (1.5%)</li>
+            <li> <span style={iconStyles}><FaCheck/></span>25 products listings</li>
+            <li> <span style={iconStyles}><FaCheck/></span>50 invoices & receipts </li>
+            <li> <span style={iconStyles}><FaCheck/></span>Unlimited discounts & coupons</li>
+            <li> <span style={iconStyles}><FaCheck/></span>50 sales record</li>
+            <li> <span style={iconStyles}><FaCheck/></span>20 messaging credits</li>
+            <li> <span style={iconStyles}><FaCheck/></span>Business Analytics</li>
+            <li> <span style={iconStyles}><FaCheck/></span>Free .bumpa.shop domain</li>
+            <li> <span style={iconStyles}><FaCheck/></span>Free .bumpa.shop domain</li>
+            <li> <span style={iconStyles}><FaCheck/></span>Free .bumpa.shop domain</li>
+          </ul>
+          <a href="/" class="btn  btn--full btn--busines">Get Started</a>
     </div>        
+
+    <div className="card">
+        <h5 className="card-title"> Bumpa Basic </h5>
+        <p className="card-price"><span class="price-tag">NO</span>/month</p>
+          <p className="description">For new businesses</p>
+          <hr className="line" />
+          <ul className="card-list">
+            <li> <span style={iconStyles}><FaCheck/></span>Free business website</li>
+            <li> <span style={iconStyles}><FaCheck/></span>Online transaction charge (1.5%)</li>
+            <li> <span style={iconStyles}><FaCheck/></span>25 products listings</li>
+            <li> <span style={iconStyles}><FaCheck/></span>50 invoices & receipts </li>
+            <li> <span style={iconStyles}><FaCheck/></span>Unlimited discounts & coupons</li>
+            <li> <span style={iconStyles}><FaCheck/></span>50 sales record</li>
+            <li> <span style={iconStyles}><FaCheck/></span>20 messaging credits</li>
+            <li> <span style={iconStyles}><FaCheck/></span>Business Analytics</li>
+            <li> <span style={iconStyles}><FaCheck/></span>Free .bumpa.shop domain</li>
+          </ul>
+          <a href="/" class="btn  btn--full btn--busines">Get Started</a>
+    </div>           
     </div>
     <h1 className='borderbottom'>compare plans and features</h1>
      </div>
