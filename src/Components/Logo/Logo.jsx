7c10427c5f24af7ebf265c675b10f49592f671db
juayuohcarineneng19 <juayuoh@gmail.com>
2023-08-20 23:28:52 +0100

    /

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import './Logo.css'
 
+
 function Logo() {
+  
   return (
-    <div className="logo-carousel">
+    <div>
+    <div className="logo-carousel"> 
       <div className="heading5">
             <h1>Bumpa in the press</h1>
-            <p>The media recognises what we are doing for small businesses in Africa.</p>
+            <p>The media recognises what we are doing for small businesses in Africa.</p> 
         </div>
+    </div>
+    <div className="carousel">
+        <div className="marquee">
+          <div className="marquee-inner">
+          <img src="image1.jpg" alt="Image 1" />
+        <img src="image2.jpg" alt="Image 2" />
+        <img src="image3.jpg" alt="Image 3" />
+        <img src="image4.jpg" alt="Image 4" />
+        <img src="image5.jpg" alt="Image 5" />
+        <img src="image6.jpg" alt="Image 6" />
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
