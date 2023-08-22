6822efc506bffe6623c3e70153da2ba6059d13a4
juayuohcarineneng19 <juayuoh@gmail.com>
2023-08-22 22:20:37 +0100

    testimonial count

--- src/Components/Testimonial/Testimonial.jsx
+++ src/Components/Testimonial/Testimonial.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './Testimonial.css'
 
 function Testimonial() {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setCount(prevCount => {
+          if (prevCount >= 50000) {
+            clearInterval(interval);
+            return prevCount;
+          }
+          return prevCount + 1000;
+        });
+      }, 100);
+      return () => clearInterval(interval);
+    }, []);
+  
+
 
     const spanStyle={
         color:'#008000'
@@ -10,10 +26,8 @@ function Testimonial() {
   return (
     <div className='testimonial'>
         <div className="heading3">
-            <h1>Trusted By Over <span style={spanStyle}>50,000</span> SME's</h1>
-            <p>Our customers talk about how Bumpa solves real life business problems for them
-
-</p>
+            <h1>Trusted By Over <span style={spanStyle}>{ count }</span> SME's</h1>
+            <p>Our customers talk about how Bumpa solves real life business problems for them</p>
         </div>
 
     </div>
