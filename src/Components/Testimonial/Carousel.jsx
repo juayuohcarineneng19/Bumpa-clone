62db8d3f29c990e79e9df4681e72842cec9426cd
juayuohcarineneng19 <juayuoh@gmail.com>
2023-08-23 23:50:04 +0100

    carousel

--- src/Components/Testimonial/Carousel.jsx
+++ src/Components/Testimonial/Carousel.jsx
@@ -2,19 +2,42 @@ import React from 'react'
 
 function Carousel() {
   return (
+    <div className="carousel-wrap">
     <div className='testimonial-carousel'>
         <div className="testimonial-card">
-            <img src="/img/steph.jpg" alt="steph testimonial" />
+            <img src="/img/steph.jpg" alt="steph testimonial"/>
         </div>
         <div className="testimony">
             <h3>"Thank You Bumpa"</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
                  Id et illum deleniti voluptas numquam unde maxime nam
                   voluptatum aperiam eos blanditiis, quae itaque,
-                 aliquid libero atque nobis illo. Voluptate, voluptatibus.</p>
-                 
+                 aliquid libero atque nobis illo. Voluptate, voluptatibus.</p>   
                  <p>Tracy Banks</p>
         </div>
+        <div className="testimonial-card">
+            <img src="/img/steph.jpg" alt="steph testimonial"/>
+        </div>
+        <div className="testimony">
+            <h3>"Thank You Bumpa"</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                 Id et illum deleniti voluptas numquam unde maxime nam
+                  voluptatum aperiam eos blanditiis, quae itaque,
+                 aliquid libero atque nobis illo. Voluptate, voluptatibus.</p>   
+                 <p>Tracy Banks</p>
+        </div>
+        <div className="testimonial-card">
+            <img src="/img/steph.jpg" alt="steph testimonial"/>
+        </div>
+        <div className="testimony">
+            <h3>"Thank You Bumpa"</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                 Id et illum deleniti voluptas numquam unde maxime nam
+                  voluptatum aperiam eos blanditiis, quae itaque,
+                 aliquid libero atque nobis illo. Voluptate, voluptatibus.</p>   
+                 <p>Tracy Banks</p>
+        </div>
+    </div>
     </div>
   )
 }
