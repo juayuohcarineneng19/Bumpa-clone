3bee5c18073937a7cd6c8b336cb539f93b140c96
juayuohcarineneng19 <juayuoh@gmail.com>
2023-09-12 23:41:53 +0100

    /

--- src/Components/head/Navbar.jsx
+++ src/Components/head/Navbar.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons'; 
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 
 import './Navbar.css'
 
 function Navbar() {
-  const [menuOpen, setMenuOpen] = useState(false) 
 
-// Toggle menu function
-const toggleMenu = () => {
-  setMenuOpen(!menuOpen)
-}
+          // State for menu open/closed 
+        const [menuOpen, setMenuOpen] = useState(false);
+
+        // Toggle menu function
+        const toggleMenu = () => {
+          setMenuOpen(!menuOpen);
+        }
 
   
   return (
@@ -17,8 +23,9 @@ const toggleMenu = () => {
         <div className="nav-logo">
         <h2 className="logo">Bumpa </h2>
       </div>
-      <div className="main-nav">
-      <ul className={`main-nav-list ${menuOpen ? 'active' : ''}`}>
+
+      <div className={`main-nav ${menuOpen ? 'active' : ''}`}>
+      <ul className="main-nav-list ">
         <li><NavLink href="#" className="main-nav-link">Features</NavLink></li>
         <li><NavLink href="#" className="main-nav-link">Community</NavLink></li>
         <li><NavLink href="#" className="main-nav-link">Pricing</NavLink></li>
@@ -26,18 +33,36 @@ const toggleMenu = () => {
         <li><NavLink href="#" className="main-nav-link nav-cta">Sell on Instagram</NavLink></li>
         
       </ul>
-      </div>
+
       <div className="navbuttons">
       <a href="/" class="btn btn--outline margin-right-sm"><strong> Login </strong></a>
          <a href="/" class="btn btn--full">Get Started</a>
       </div>
-      <div className="hamburger" onClick={() => toggleMenu()}>
-          <div class="line"></div>
-            <div class="line"></div>
-               <div class="line"></div>
-        </div>
+      </div>
+ 
+
+
+                            <div className="menu-icons">
+
+                      {/* Hamburger Icon */}
+                       <FontAwesomeIcon 
+                        icon={faBars}
+                        onClick={toggleMenu} 
+                        className={!menuOpen ? 'menu-icon' : 'hide'} 
+                      /> 
+
+                      {/* Close Icon */}
+                       <FontAwesomeIcon
+                        icon={faTimes}  
+                        onClick={toggleMenu}
+                        className={menuOpen ? 'menu-icon' : 'hide'} 
+                      />
+
+                      </div> 
+
 
     </div>
+
   )
 }
 
